69c21d03e1f3f3f141ed91eb19639ed68438c025
githubofjk <hkim317@gmail.com>
2021-01-23 20:40:24 -0800

    display hardcoded boardArray

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 const gameBoard = (() => {
     const boardArray = [];
-
+    for (i = 0; i < 9; i++) {
+        boardArray[i] = "X";
+    }
     return {boardArray};
 })();
 
+const boardSquares = Array.from(document.querySelectorAll(".boardSquare"));
+boardSquares.forEach(square => square.addEventListener("click", displayGameBoard));
 
+
+function displayGameBoard(e) {
+    boardSquares[e.target.id].textContent = gameBoard.boardArray[e.target.id];
+}
