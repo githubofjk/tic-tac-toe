ce0a8543b0efe304dafaf4b195a75005a3446e8e
githubofjk <hkim317@gmail.com>
2021-01-24 22:47:04 -0800

    Update scripts.js game.checkWinner()

--- scripts.js
+++ scripts.js
@@ -13,7 +13,17 @@ const game = (() => {
     
     let turnCounter = 0;
 
-    return {player1, player2, turnCounter};
+    const checkWinner = () => {
+        if (gameBoard.boardArray[0] !== undefined &&
+            gameBoard.boardArray[0] === gameBoard.boardArray[1] &&
+            gameBoard.boardArray[0] === gameBoard.boardArray[2] &&
+            gameBoard.boardArray[1] === gameBoard.boardArray[2]) 
+        {
+            return true;
+        }
+    };
+
+    return {player1, player2, turnCounter, checkWinner};
 })();
 
 const boardSquares = Array.from(document.querySelectorAll(".boardSquare"));
@@ -21,9 +31,7 @@ boardSquares.forEach(square => square.addEventListener("click", displayControlle
 
 function displayController(e) {
     const displayWon = document.querySelector("#won");
-    console.log(game.player1Won);
-    console.log(gameBoard.boardArray);
-
+    
     // player1 or player2's turn
     if (game.turnCounter % 2 === 0) {
         // check if square already has a value
@@ -31,7 +39,7 @@ function displayController(e) {
             gameBoard.boardArray[e.target.id] = "X";
             boardSquares[e.target.id].textContent = gameBoard.boardArray[e.target.id];
             // check if won
-            if (gameBoard.boardArray[0] === gameBoard.boardArray[1] && gameBoard.boardArray[0] === gameBoard.boardArray[2]) {
+            if (game.checkWinner()) {
                 displayWon.textContent = `${game.player1.name} won!`;
             }
             // if didn't win, then next players turn
@@ -44,7 +52,13 @@ function displayController(e) {
         if (boardSquares[e.target.id].textContent === "") {
             gameBoard.boardArray[e.target.id] = "0";
             boardSquares[e.target.id].textContent = gameBoard.boardArray[e.target.id];
-            game.turnCounter++;
+            if (game.checkWinner()) {
+                displayWon.textContent = `${game.player2.name} won!`;
+            }
+            // if didn't win, then next players turn
+            else {
+                game.turnCounter++;
+            }
         }
     }
 }
